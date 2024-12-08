76c288b67628499419c26438dd81e2507aaa8eb4
Alex Westphal <alexwestphal.nz@gmail.com>
2024-12-08 14:57:41 +1300

    Removed api as a protected route.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/api(.*)', '/manage(.*)', '/unified(.*)'])
+const isProtectedRoute = createRouteMatcher(['/manage(.*)', '/unified(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
     if(isProtectedRoute(req)) await auth.protect()
