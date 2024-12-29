c62a4bcb9a9a061eaeee15d18f002def9f6fc5dd
Alex Westphal <alexwestphal.nz@gmail.com>
2024-12-30 07:45:49 +1300

    Fixed example competency reports.

--- src/app/competencies/reports/individual/page.tsx
+++ src/app/competencies/reports/individual/page.tsx
@@ -11,7 +11,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from '@/components/ui/table'
 
 import { usePersonnelQuery } from '@/lib/api/personnel'
-import { useSkillsTreeQuery } from '@/lib/api/skills'
+import { useSkillPackagesQuery } from '@/lib/api/skills'
 
 import * as Paths from '@/paths'
 
@@ -21,7 +21,7 @@ const Statuses = ['', 'Highly Confident', 'Competent', 'Competent', 'Competent',
 
 export default function IndividualReportPage() {
 
-    const skillsTreeQuery = useSkillsTreeQuery()
+    const skillPackagesQuery = useSkillPackagesQuery()
     const personnelQuery = usePersonnelQuery()
 
     return <AppPage 
@@ -35,23 +35,22 @@ export default function IndividualReportPage() {
             <PageTitle>Individual Report</PageTitle>
             <PageDescription>{`Competency report for member 'John Smith'. Generated ${format(new Date(), 'PPP')}`}</PageDescription>
         </PageHeader>
-        { (skillsTreeQuery.isPending || personnelQuery.isPending) ? <div className="flex flex-col items-stretch gap-2">
+        { (skillPackagesQuery.isPending || personnelQuery.isPending) ? <div className="flex flex-col items-stretch gap-2">
                 <Skeleton className="h-8"/>
                 <Skeleton className="h-8"/>
                 <Skeleton className="h-8"/>
                 <Skeleton className="h-8"/>
                 <Skeleton className="h-8"/>
         </div>: null}
-        { (skillsTreeQuery.isSuccess && personnelQuery.isSuccess) ? <Accordion type="single" collapsible>
-            {skillsTreeQuery.data.map(capability => {
+        { (skillPackagesQuery.isSuccess && personnelQuery.isSuccess) ? <Accordion type="single" collapsible>
+            {skillPackagesQuery.data.map(skillPackage => {
                 
-                const skillsInCapability = capability.skillGroups.flatMap(skillGroup => skillGroup.skills)
-                const skillCount = skillsInCapability.length
+                const skillCount = skillPackage.skills.length
                 const selectedCount = _.random(0, skillCount)
 
-                return <AccordionItem key={capability.id} value={capability.id}>
+                return <AccordionItem key={skillPackage.id} value={skillPackage.id}>
                     <AccordionTrigger>
-                        <div className="flex-grow text-left">{capability.name}</div>
+                        <div className="flex-grow text-left">{skillPackage.name}</div>
                         <div className="text-xs text-muted-foreground mr-4">{selectedCount} of {skillCount} current</div>
                     </AccordionTrigger>
                     <AccordionContent>
@@ -66,25 +65,28 @@ export default function IndividualReportPage() {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {capability.skillGroups.map(skillGroup => {
+                                {skillPackage.skillGroups.map(skillGroup => {
                                     return <React.Fragment key={skillGroup.id}>
                                         <TableRow>
                                             <TableCell className="font-semibold">{skillGroup.name}</TableCell>
                                         </TableRow>
                                         
-                                        {skillGroup.skills.map(skill => {
-                                            const status = Statuses[_.random(0, Statuses.length-1)]
-                                            const date = subDays(new Date(), _.random(0, 400))
-                                            const assessor = personnelQuery.data[_.random(0, personnelQuery.data.length-1)]
+                                        {skillPackage.skills
+                                            .filter(skill => skill.skillGroupId == skillGroup.id)
+                                            .map(skill => {
+                                                const status = Statuses[_.random(0, Statuses.length-1)]
+                                                const date = subDays(new Date(), _.random(0, 400))
+                                                const assessor = personnelQuery.data[_.random(0, personnelQuery.data.length-1)]
 
-                                            return <TableRow key={skill.id}>
-                                                <TableCell></TableCell>
-                                                <TableCell>{skill.name}</TableCell>
-                                                <TableCell>{status}</TableCell>
-                                                <TableCell>{status && format(date, 'yyyy-MM-dd')}</TableCell>
-                                                <TableCell className="text-muted-foreground">{status && assessor.name}</TableCell>
-                                            </TableRow>
-                                        })}
+                                                return <TableRow key={skill.id}>
+                                                    <TableCell></TableCell>
+                                                    <TableCell>{skill.name}</TableCell>
+                                                    <TableCell>{status}</TableCell>
+                                                    <TableCell>{status && format(date, 'yyyy-MM-dd')}</TableCell>
+                                                    <TableCell className="text-muted-foreground">{status && assessor.name}</TableCell>
+                                                </TableRow>
+                                            })
+                                        }
                                     </React.Fragment>
                                 })}
                             </TableBody>
--- src/app/competencies/reports/teamSkills/page.tsx
+++ src/app/competencies/reports/teamSkills/page.tsx
@@ -10,14 +10,14 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/
 import { Skeleton } from '@/components/ui/skeleton'
 import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from '@/components/ui/table'
 
-import { useSkillsTreeQuery } from '@/lib/api/skills'
+import { useSkillPackagesQuery } from '@/lib/api/skills'
 
 import * as Paths from '@/paths'
 
 
 export default function TeamSkillsReportPage() {
 
-    const skillsTreeQuery = useSkillsTreeQuery()
+    const skillPackagesQuery = useSkillPackagesQuery()
 
     return <AppPage 
         label="Team Skills"
@@ -30,19 +30,19 @@ export default function TeamSkillsReportPage() {
             <PageTitle>Team Skills Report</PageTitle>
             <PageDescription>{`Skills report for Team NZ-RT13. Generated ${format(new Date(), 'PPP')}`}</PageDescription>
         </PageHeader>
-        { (skillsTreeQuery.isPending) ? <div className="flex flex-col items-stretch gap-2">
+        { (skillPackagesQuery.isPending) ? <div className="flex flex-col items-stretch gap-2">
                 <Skeleton className="h-8"/>
                 <Skeleton className="h-8"/>
                 <Skeleton className="h-8"/>
                 <Skeleton className="h-8"/>
                 <Skeleton className="h-8"/>
         </div>: null}
-        { (skillsTreeQuery.isSuccess) ? <Accordion type="single" collapsible>
-            {skillsTreeQuery.data.map(capability => {
+        { (skillPackagesQuery.isSuccess) ? <Accordion type="single" collapsible>
+            {skillPackagesQuery.data.map(skillPackage => {
 
-                return <AccordionItem key={capability.id} value={capability.id}>
+                return <AccordionItem key={skillPackage.id} value={skillPackage.id}>
                     <AccordionTrigger>
-                        <div className="flex-grow text-left">{capability.name}</div>
+                        <div className="flex-grow text-left">{skillPackage.name}</div>
                         {/* <div className="text-xs text-muted-foreground mr-4">{selectedCount} of 25 members current</div> */}
                     </AccordionTrigger>
                     <AccordionContent>
@@ -55,21 +55,22 @@ export default function TeamSkillsReportPage() {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {capability.skillGroups.map(skillGroup => {
+                                {skillPackage.skillGroups.map(skillGroup => {
                                     return <React.Fragment key={skillGroup.id}>
                                         <TableRow>
                                             <TableCell className="font-semibold">{skillGroup.name}</TableCell>
                                         </TableRow>
                                         
-                                        {skillGroup.skills.map(skill => {
-                                           
-
-                                            return <TableRow key={skill.id}>
-                                                <TableCell></TableCell>
-                                                <TableCell>{skill.name}</TableCell>
-                                                <TableCell>{_.random(0, 100)}%</TableCell>
-                                            </TableRow>
-                                        })}
+                                        {skillPackage.skills
+                                            .filter(skill => skill.skillGroupId == skillGroup.id)
+                                            .map(skill => {
+                                                return <TableRow key={skill.id}>
+                                                    <TableCell></TableCell>
+                                                    <TableCell>{skill.name}</TableCell>
+                                                    <TableCell>{_.random(0, 100)}%</TableCell>
+                                                </TableRow>
+                                            })
+                                        }
                                     </React.Fragment>
                                 })}
                             </TableBody>
